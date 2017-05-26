acb9278bf51673111aed93e8e3bf72f3a1cb624f
Roman Provazník <dzoukr@dzoukr.cz>
2017-05-26 13:28:37 +0200

    simplyfied build with AspNetCoreModule

--- src/AzureAppServiceTest/App.fs
+++ src/AzureAppServiceTest/App.fs
@@ -16,7 +16,7 @@ let main argv =
         choose [
             path "/test2" >=> OK "Ha! F# web routing run"
             path "/test" >=> OK "Ha! F# web routing run"
-            path "/" >=> OK "Ha! F# web right from GitHub with autoredeploy"
+            path "/" >=> OK "Ha! F# web right from GitHub with autoredeploy and AspNetCoreModule"
         ]
 
     // start service server
